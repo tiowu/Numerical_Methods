699c5e55fdfdf3f53a7ad25c0d699c2dd1b97a74
tiowu <tiowuuuu@gmail.com>
2024-11-19 23:37:20 -0500

    improved code

--- Lagrange Interpolation Method.py.py
+++ Lagrange Interpolation Method.py.py
@@ -1,17 +1,18 @@
 x = [0, 20, 40, 60, 80, 100]
 y = [26.0, 48.6, 61.6, 71.2, 74.8, 75.2]
 
-m = len(x)
-n = m-1
+n = len(x)
 
-xp = float(input('Enter x : '))
-yp = 0
+xp = float(input("Enter the value of x: "))
 
-for i in range(n+1):
-    L = 1
-    for j in range(n+1):
+yp = 0.0
+
+#do Lagrange interpolation
+for i in range(n):
+    L = 1.0  #initialize the Lagrange basis polynomial
+    for j in range(n):
         if j != i:
-            L *= (xp - x[j])/(x[i] - x[j])
-    yp += y[i]*L
-    
-print('For x = %.1f, y = %.1f' % (xp,yp))
+            L *= (xp - x[j]) / (x[i] - x[j])  #compute L_i(x)
+    yp += y[i] * L  #add contribution of L_i(x) to yp
+
+print(f"For x = {xp:.1f}, the interpolated y = {yp:.1f}")
